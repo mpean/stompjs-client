29e8291810e62e2128f7b6014a5af0cd121209d7
Maxime Pean <maxpean@gmail.com>
2015-12-15 16:17:11 +0100

    test

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ var client = Stomp.overTCP('localhost', 61613);
 // uncomment to print out the STOMP frames
  client.debug = console.log;
 
-client.connect('user', 'user', function(frame) {
+ client.connect('user', 'user', function(frame) {
   console.log('connected to Stomp');
 
-  client.subscribe('/queue/myqueue', function(message) {
-    console.log("received message " + message.body);
+  client.subscribe('/queue/test', function(message) {
+    var now =new Date();
+    console.log(now.toJSON()+" - received message " + message.body);
+
+    var jmessage = JSON.parse(message.body);
+    console.log("time message: "+jmessage.time);
+    console.log("message body: "+jmessage.body);
 
     // once we get a message, the client disconnects
-    client.disconnect();
+    //client.disconnect();
   });
 
-  console.log ('sending a message');
-  client.send('/queue/myqueue', {}, 'Hello, node.js!');
+  //console.log ('sending a message');
+  //client.send('/queue/myqueue', {}, 'Hello, node.js!');
 });
